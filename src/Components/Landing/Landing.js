dec4d6c6fdca5a677fb1e5db71320d3d23a3a58b
unknown <etch.rush@gmail.com>
2018-11-12 23:01:39 -0800

    Started working on landing page

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import "../Landing/Landing.css";
+
+class Landing extends Component {
+
+    render() {
+        return (
+            <div className="background container">
+                <div className="nav">
+                    <a className="home" href="">
+                        James Etchells
+                    </a>
+                    <a className="projects">
+                        Projects
+                    </a>
+                    <a className="contact">
+                        Contact
+                    </a>
+                </div>
+                <div className="welcome">
+                    <h1 className="name">James Etchells</h1>
+                    <p>Full-Stack Web Developer</p>
+                </div>
+                <div className="about">
+                    <div className="technologies">
+                        <ul>
+                            <h1>
+                                Browser Based Technologies
+                            </h1>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>jQuery</li>
+                            <li>Responsive Design</li>
+                            <li>Bootstrap</li>
+                            <li>Handlebars</li>
+                            <li>Firebase</li>
+                            <li>Cookies, Local Storage</li>
+                            <li>React.js</li>
+                        </ul>
+                        <ul>
+                            <h1>
+                                Deployment
+                            </h1>
+                            <li>Heroku</li>
+                            <li>Git</li>
+                        </ul>
+                        <ul>
+                            <h1>
+                                Databases
+                            </h1>
+                            <li>MySQL</li>
+                            <li>MongoDB</li>
+                        </ul>
+                        <ul>
+                            <h1>
+                                Node.Js Server Side Deployment
+                            </h1>
+                            <li>Express</li>
+                            <li>Security and Session Storage</li>
+                            <li>User Authentication</li>
+                            <li>MERN Stack</li>
+                        </ul>
+
+                    </div>
+                    <div className="footer">
+                        <p>&copy; James Etchells</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
---
+++ src/Components/Projects/Projects.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../Landing/Landing.css";
+
+class Projects extends Component {
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+};
+
+export default Projects
