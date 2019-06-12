b94a187c7b8ff923e4a923087113215f652b423d
Etch <Etch.rush@gmail.com>
2019-06-12 13:15:56 -0800

    contact page under construction

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,18 +5,22 @@ import Nav from "../Nav";
 class Contact extends Component {
     render() {
         return (
-            <div className="contactBody">
-                <Nav></Nav>
-                <form id="contact-form" method="POST">
-                    <h1>Shoot me an Email!</h1>
-                    <h2>Name</h2>
-                    <input type="text"></input>
-                    <h2>Email</h2>
-                    <input type="text"></input>
-                    <h2>Message</h2>
-                    <textarea rows="5" width="200px"></textarea>
-                    <div><button id="submit" type="submit">Submit</button></div>
-                </form>
+            <div className = "contact">
+                <div className = "contact-title">
+                    <h1 className="contact-title-header">
+                        James Etchells
+                    </h1>
+                    <p>
+                        Information to get in touch with me
+                    </p>
+                </div>
+                <div className="contact-info">
+                    Email
+                    phone
+                    linkedIN
+                    github
+                    upwork
+                </div>
             </div>
         )
     }
