801054a28ac3c603adc075558e24835746abc7dd
unknown <etch.rush@gmail.com>
2020-06-16 12:30:55 -0700

    updated background color on bio section

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -11,13 +11,16 @@ class Landing extends Component {
                 <div className="welcome">
                     <h1 className="name">James Etchells</h1>
                     <h2>Full-Stack Web Developer</h2>
-                    <p className="bio">Hey, I'm a Full-Stack Developer, looking to be a valuable member of a team. I have a wide range of tech knowledge, and building computers has been a passion of mine since I was a kid. I come from a background of hard labour jobs such as a cooking in some of the best kitchens in San Diego. One of my biggest strengths is adapting, becoming whatever the team needs to be progress. I find a lot of motivation helping others, and believe together people can achieve exceptional things. I am always continuing my education in all things tech via online courses, and my goal is to become OSCP certified and practice pen testing. I look forwarding to hearing about new projects, and if I can be apart of them.Thank you for reading, be well and stay safe. </p>
+                    <div className="bio">
+                        <p>Hey, I'm a Full-Stack Developer, looking to be a valuable member of a team. I have a wide range of tech knowledge, and building computers has been a passion of mine since I was a kid. I come from a background of hard labour jobs such as a cooking in some of the best kitchens in San Diego. One of my biggest strengths is adapting, becoming whatever the team needs to progress. I find a lot of motivation helping others, and believe together people can achieve exceptional things. I am always continuing my education in all things tech via online courses, and my goal is to become OSCP certified and practice pen testing. I look forwarding to hearing about new projects, and if I can be apart of them.Thank you for reading, be well and stay safe. </p>
+                    </div>
+                    
                     <div className="links">
                         <Link path="exact" to="/Github">Github</Link>
                         <Link path="exact" to="/LinkedIn">LinkedIn</Link>
                         <a href="www.gmail.com">UpWork</a>
                     </div>
-                    <p>Etch.rush@gmail.com</p>
+                    <p className="gmail">Etch.rush@gmail.com</p>
                 </div>
                 <div className="about">
                     <div className="technologies">
